0a980898b2822c762cff10efe771b15a3101078c
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-06-15 13:48:16 -0300

    criando service de evento

--- backend/src/ProEventos.Persistence/Contratos/IEventoPersist.cs
+++ backend/src/ProEventos.Persistence/Contratos/IEventoPersist.cs
@@ -8,8 +8,8 @@ namespace ProEventos.Persistence.Contratos
 {
     public interface IEventoPersist
     {
-        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
-        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
-        Task<Evento> GetEventoByIdAsync(int EventoId, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
+        Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrantes = false);
     }
 }
