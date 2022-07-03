25ebf071e6f7389deb190c99a69f0ac3ef23dcdb
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-03 18:47:13 -0300

    criando validações para formulário de perfil

--- frontend/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ frontend/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = this.formBuilder.group({})
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmaSenha')
+    }
+
+    this.form = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required],
+      funcao: ['', Validators.required],
+      descricao: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmaSenha: ['', Validators.required],
+    }, formOptions)
+
   }
 
+  get f(): any {
+    return this.form.controls
+  }
+
+  public resetForm(): void {
+    this.form.reset()
+  }
 }
